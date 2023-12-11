375b01b6b94a71ce19e6fa9ee2f1472a0d051868
Yolimar Rios <yolimar.rios23@gmail.com>
2023-12-11 15:17:24 -0500

    added comments

--- backend/books/router.py
+++ backend/books/router.py
@@ -5,6 +5,7 @@ app_name = 'books'
 
 router = routers.DefaultRouter()
 
+# register book, author and category routes
 router.register(r'book', BookViewSet, basename='book')
 router.register(r'author', AuthorViewSet, basename='author')
 router.register(r'category', CategoryViewSet, basename='category')
